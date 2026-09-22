6ba77e07e0b3ba61190d1a3a8372ea298f62e62c
sharma11ji <amrjkumar848502@gmail.com>
2026-09-22 19:49:54 +0530

    Register PWA service worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,1 @@
-import React from"react";import{createRoot}from"react-dom/client";import"./styles.css";import App from"./App.jsx";createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
+import React from"react";import{createRoot}from"react-dom/client";import"./styles.css";import App from"./App.jsx";import{registerPWA}from"./pwa.js";registerPWA();createRoot(document.getElementById("root")).render(<React.StrictMode><App/></React.StrictMode>);
